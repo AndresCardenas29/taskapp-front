19874db42c824d21bff8acfc0b93c25264f7c86b
Andrés Cárdenas <mandrescardenash@gmail.com>
2025-05-27 15:07:25 -0500

    feat: add favicon to application header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,14 @@ export default defineNuxtConfig({
 	compatibilityDate: "2025-05-15",
 	devtools: { enabled: true },
 
+	app: {
+		head: {
+			link: [
+				{ rel: 'icon', type: 'image/x-icon', href: '/icon.ico' }
+			]
+		}
+	},
+
 	modules: [
 		"@nuxt/eslint",
 		"@nuxt/fonts",
