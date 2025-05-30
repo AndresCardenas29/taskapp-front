7e6e89905a287cdc12de85c5e8c5bcda045ade80
Andrés Cárdenas <mandrescardenash@gmail.com>
2025-05-30 10:55:06 -0500

    path backend

--- config/env.ts
+++ config/env.ts
@@ -1,2 +1,3 @@
 export const NODE_ENV = process.env.NODE_ENV ?? "development";
-export const BACKEND_URL = process.env.BACKEND_URL ?? "http://localhost:4000";
+export const BACKEND_URL =
+	process.env.BACKEND_URL ?? "https://backend.nekdress.online/tasks";
