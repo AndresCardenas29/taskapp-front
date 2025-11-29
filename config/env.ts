ee5c13734687a44db19e0ca6bbf666080019b5bd
Andrés Cárdenas <mandrescardenash@gmail.com>
2025-11-28 23:52:16 -0500

    fix: update BACKEND_URL to use local development server

--- config/env.ts
+++ config/env.ts
@@ -1,3 +1,3 @@
 export const NODE_ENV = process.env.NODE_ENV ?? "development";
 export const BACKEND_URL =
-	process.env.BACKEND_URL ?? "https://backend.nekdress.online";
+	process.env.BACKEND_URL ?? "127.0.0.1:4000";
